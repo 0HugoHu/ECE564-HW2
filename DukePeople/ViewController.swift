ddd1ced2bc3c89da77576db5e38ed8d017935ea3
yh342 <yh342@duke.edu>
2023-08-31 16:42:53 -0400

    Initial Commit

---
+++ DukePeople/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  DukePeople
+//
+//  Created by Hugooooo on 8/31/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
